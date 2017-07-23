9141bd2de73b0cf7d000e6848e4712a88591e7ab
mxdavis <md01@me.com>
2017-07-23 23:34:59 +0300

    test hour and decimal for bill_time

--- timer-billing-api/spec/models/task_spec.rb
+++ timer-billing-api/spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
-
+require 'pry'
+   
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   
+   describe 'validations' do
+
+    it 'converts hours to decimal before save' do
+      task = build(:task, bill_time: '1:30:00')
+      task.save
+
+      expect(task.bill_time).to eq("1.5")
+    end
+
+    it 'accepts decimal and saves' do
+      task = build(:task, bill_time: 1.5)
+      task.save
+
+      expect(task.bill_time).to eq("1.5")
+    end
+
+  end
 end
