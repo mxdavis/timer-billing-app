ba992a8a037dd759f0f83d280e7a5144e46d0c29
mxdavis <md01@me.com>
2017-07-23 23:41:05 +0300

    company validation tests

--- timer-billing-api/spec/models/company_spec.rb
+++ timer-billing-api/spec/models/company_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+    describe 'validations' do
+
+    it 'requires a name' do
+      company = build(:company, name: nil)
+      expect(company.valid?).to equal(false)
+      expect(company.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'requires an email' do
+      company = build(:company, email: nil)
+      expect(company.valid?).to equal(false)
+      expect(company.errors.full_messages).to include("Email is invalid")
+    end
+
+    it 'requires a valid email' do
+      company1 = build(:company, email: 'firstname@a.')
+      company2 = build(:company, email: 'firstname@company')
+      company3 = build(:company, email: 'firstname.com')
+
+      expect(company1.valid?).to equal(false)
+      expect(company2.valid?).to equal(false)
+      expect(company3.valid?).to equal(false)
+      expect(company1.errors.full_messages).to include("Email is invalid")
+    end
+
+  end
 end
