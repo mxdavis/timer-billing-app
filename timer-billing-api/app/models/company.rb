72b5517bd30733d28b164c3af402bf7b8b9179ab
mxdavis <md01@me.com>
2017-07-23 20:59:25 +0300

    validates format of user

--- timer-billing-api/app/models/company.rb
+++ timer-billing-api/app/models/company.rb
@@ -4,4 +4,5 @@ class Company < ApplicationRecord
   has_many :tasks, through: :projects
 
   validates :name, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
--- timer-billing-api/app/models/user.rb
+++ timer-billing-api/app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :name, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
