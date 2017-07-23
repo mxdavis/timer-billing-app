3501559e6ca8353e5ed218fbe7614ea0fef069c4
mxdavis <md01@me.com>
2017-07-23 23:45:57 +0300

    validations

--- timer-billing-api/spec/models/project_spec.rb
+++ timer-billing-api/spec/models/project_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires a name' do
+      project = build(:project, name: nil)
+      expect(project.valid?).to equal(false)
+      expect(project.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
