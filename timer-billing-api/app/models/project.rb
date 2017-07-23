9ced26e30b629ffab96abba64a9b7573f35b296e
mxdavis <md01@me.com>
2017-07-24 00:33:21 +0300

    add validation for client and tests

--- timer-billing-api/app/models/project.rb
+++ timer-billing-api/app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :client
   has_many :tasks
 
+  validates :client, presence: true
   validates :name, presence: true
 end
--- timer-billing-api/spec/models/project_spec.rb
+++ timer-billing-api/spec/models/project_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Project, type: :model do
       expect(project.valid?).to equal(false)
       expect(project.errors.full_messages).to include("Name can't be blank")
     end
+
+    it 'requires a client' do
+      project = build(:project, client: nil)
+      expect(project.valid?).to equal(false)
+      expect(project.errors.full_messages).to include("Client can't be blank")
+    end
   end
   describe 'relationships' do
     
@@ -16,7 +22,11 @@ RSpec.describe Project, type: :model do
       expect(project.client.name).to eq("Apple Co.")
     end
 
-    it 'has many projects'
+    it 'has many tasks' do
+      project = create(:project)
+
+      expect(project.tasks).not_to eq(nil)
+    end
 
   end
 end
