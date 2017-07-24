d558d43491e07dbf343197231b6009c5c5c5a89e
mxdavis <md01@me.com>
2017-07-24 22:53:25 +0300

    create routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api do 
     namespace :v1 do
-
-
+      resources :users do
+        resources :clients do 
+          resources :tasks
+          end
+      end
 
     end
   end
