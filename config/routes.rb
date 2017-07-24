f049ed0010e906f7872667774acd2fb16c75d1f1
mxdavis <md01@me.com>
2017-07-25 01:09:58 +0300

    add unbilled and billed task routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :users do
-        resources :tasks, only: [:index]
-        resources :clients do 
-          resources :tasks, except: [:index]
-          end
+        get '/unbilled_tasks' => 'tasks#unbilled'
+        get '/billed_tasks' => 'tasks#billed'
+        resources :tasks, :clients
       end
-
     end
   end
 end
