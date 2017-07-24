c518ce67e371dc56c7cf44cb191dd5c79aecf71a
mxdavis <md01@me.com>
2017-07-25 01:21:52 +0300

    billed and unbilled tasks api working with all data necessary to render previously saved tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,25 +1,35 @@
 class Api::V1::TasksController < ApplicationController
   
   def billed
-    # need api here that includes has tasks including the project and client name
-    # user = User.find_by(id: params[:user_id])
-    # json.array!(@users) do |user|
-    # json.name user.name
-
-    # json.reservations do
-    #     json.array!(user.reservations) do |reservation|
-    #         json.restaurant reservation.restaurant.name
-    #         json.reservation_time reservation.time
-    #         json.name user.name
-    #     end
-    # end
-# end
+    render json: tasks(true)
   end
 
   def unbilled
+    render json: tasks(false)
   end
 
   def create
     binding.pry
+  end
+
+  private
+
+  def tasks(billed_status)
+    Task.newest.select do |task|
+      if task.billed == billed_status
+        {
+          client: task.project.client.name,
+          client_id: task.project.client.id,
+          project: task.project.name,
+          project_id: task.project.id,
+          bill_time: task.bill_time,
+          bill_rate: task.bill_rate,
+          description: task.description,
+          total: task.total,
+          billed: task.billed, 
+          date: task.date
+        }
+      end
+    end
   end
 end
