207c15a18ab58c471bbbba2a76b0bbe7eb7a2d26
mxdavis <md01@me.com>
2017-07-25 00:10:25 +0300

    add create method

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::ClientsController < ApplicationController
-  #  #before authenticate if current user == User.find_by(id: params[:user_id])
+  #  #before authenticate if current user == User.find_by(id: params[:user_id]) 
+  #  before all find user
   def index
     user = User.find_by(id: params[:user_id])
     render json: user.clients.to_json(include: [ :projects])
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    client = user.clients.build(client_params)
+    if client.save
+      render json: { id: client_id, success: 'ok'}
+    else
+      render json: { message: "error, #{client.errors}"}, status: 412
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :email)
   end
 end
