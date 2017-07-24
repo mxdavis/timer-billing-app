ea4e8fdc5dc7522f968404fc6d6acaf4a364a548
mxdavis <md01@me.com>
2017-07-24 23:57:47 +0300

    have user api to show user details

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,26 @@
 class Api::V1::UsersController < ApplicationController
 
+  def show
+    user = User.find_by(id: params[:user_id])
+    render json: user
+  end
+  
   def create
-    user = User.new(user_params)
-    if user.save
-      render json:{
-        user: user
-        # token: user.auth?
-        # http://instruction.learn.co/video_lectures/136 first 40 minutes or so from 20 minutes is user auth
-      }, status: 500
-    end
+    # user = User.new(user_params)
+    # if user.save
+    #   render json:{
+    #     user: user
+    #     # token: user.auth?
+    #     # http://instruction.learn.co/video_lectures/136 first 40 minutes or so from 20 minutes is user auth
+    #   }, status: 500
+    # end
 
-    else
-      # render edit page with the current values so they can fill in the rest of the required fields
-      render json:{
-        errors: user.errors
-      }, status: 500
-    end
-      
+    # else
+    #   # render edit page with the current values so they can fill in the rest of the required fields
+    #   render json:{
+    #     errors: user.errors
+    #   }, status: 500
+    # end      
   end
 
   private
