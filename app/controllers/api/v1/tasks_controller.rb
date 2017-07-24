be10b2ddb864a9e89ac574bb1e51bb6e49b6a22b
mxdavis <md01@me.com>
2017-07-25 00:10:39 +0300

    spec out what i want for tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,4 +1,25 @@
 class Api::V1::TasksController < ApplicationController
+  
+  def billed
+    # need api here that includes has tasks including the project and client name
+    # user = User.find_by(id: params[:user_id])
+    # json.array!(@users) do |user|
+    # json.name user.name
 
+    # json.reservations do
+    #     json.array!(user.reservations) do |reservation|
+    #         json.restaurant reservation.restaurant.name
+    #         json.reservation_time reservation.time
+    #         json.name user.name
+    #     end
+    # end
+# end
+  end
 
+  def unbilled
+  end
+
+  def create
+    binding.pry
+  end
 end
