4f0cc33e70f4b12ee76dd088f1520740151126f3
mxdavis <md01@me.com>
2017-07-24 23:58:11 +0300

    move around routes, will prob move around again once i get authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :users do
+        resources :tasks, only: [:index]
         resources :clients do 
-          resources :tasks
+          resources :tasks, except: [:index]
           end
       end
 
