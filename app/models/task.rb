2c65b7e19d38952935642ea98acce609243751e2
mxdavis <md01@me.com>
2017-07-25 01:44:30 +0300

    remove require pry

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Task < ApplicationRecord
   belongs_to :project
   
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
-require 'pry'
-   
+  
 RSpec.describe Task, type: :model do
    
    describe 'validations' do
