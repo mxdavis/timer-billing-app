b66fd26c6acc6851e437c9ade271dcc3faa214be
mxdavis <md01@me.com>
2017-07-25 01:04:36 +0300

    update date to take date instead of datetime

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170723160532) do
     t.text "description"
     t.boolean "billed", default: false
     t.float "total"
-    t.datetime "date"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
