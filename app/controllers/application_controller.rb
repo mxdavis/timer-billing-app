1e64535a034b0d8923e46d50d1ef5301f21a3e56
mxdavis <md01@me.com>
2017-07-24 23:57:16 +0300

    took out protect from forgery for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?, :can_edit?
 
   def current_user
