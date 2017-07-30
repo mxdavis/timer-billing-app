a9c1f37cc3b0d6fc2e0960e0737eb5cab6f6b9a1
mxdavis <md01@me.com>
2017-07-30 05:38:28 +0300

    creates new project based on client

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ProjectsController < ApplicationController
+
+  def create
+    if client = Client.find_by(id: params[:clientId])
+      project = client.projects.build(project_params)
+      if project.save
+        render json: { id: project, success: 'ok'}
+      else
+        render json: { message: "error, #{project.errors}"}, status: 412
+      end
+    else 
+      render json: { message: "no project found for this project"}, status: 412
+    end
+  end
+
+  private
+
+  def project_params
+    params.permit(:name, :bill_rate)
+  end
+end
