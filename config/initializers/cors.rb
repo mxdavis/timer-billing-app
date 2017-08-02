4e5bbd8c1b25a09b0f62e0b6c01157e810420818
mxdavis <md01@me.com>
2017-08-02 19:01:43 +0300

    update cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://bill-timer-client.herokuapp.com/'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://bill-timer-client.herokuapp.com', 'http://bill-timer-client.herokuapp.com'
 
     resource '*',
       headers: :any,
